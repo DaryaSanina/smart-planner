fbfbf95d8da9455f9ff11b8cb7710fc848fc41bd
Daria Sanina <dasha.sanina.07@gmail.com>
2024-01-02 12:25:07 +0000

    Add stochastic gradient descent optimiser

---
+++ prototype/optimisers.py
@@ -0,0 +1,41 @@
+from autograd import Tensor
+
+
+class SGD:
+    """
+    Implements stochastic gradient descent.
+
+    Attributes
+    ----------
+    parameters : list[Tensor]
+        The model's parameters.
+    alpha : float
+        The model's learning rate.
+    """
+
+
+class SGD:
+    def __init__(self, parameters: list[Tensor], alpha: float = 0.1) -> None:
+        self.parameters = parameters
+        self.alpha = alpha
+    
+    def zero(self) -> None:
+        """
+        Resets the parameters' gradients.
+        """
+        for p in self.parameters:
+            p.grad.data *= 0
+    
+    def step(self, zero=True) -> None:
+        """
+        Updates the model's parameters.
+
+        Parameters
+        ----------
+        zero : bool
+            Whether the parameters' gradients should be reset after this step.
+        """
+        for p in self.parameters:
+            p.data -= p.grad.data * self.alpha
+            if zero:
+                p.grad.data *= 0
