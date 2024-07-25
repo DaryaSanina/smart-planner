2df45453c49cdf50fe75d7b7fa5ddbdfe642ac1c
Daria Sanina <dasha.sanina.07@gmail.com>
2024-07-25 13:22:05 +0100

    Add the ability to add a task to the database

--- database-server/app.py
+++ database-server/app.py
@@ -6,14 +6,32 @@ import mysql.connector
 from dotenv import load_dotenv
 import os
 import re
+from pydantic import BaseModel
+import datetime
 
 app = FastAPI()
 handler = Mangum(app)
 cursor = None
 
 
+class User(BaseModel):
+     username: str
+     email: str
+     password_hash: str
+
+
+class Task(BaseModel):
+     name: str
+     description: str | None
+     deadline: datetime.datetime | None  # In a POST request, it should be a string of the following format: "YYYY-MM-DD[T]HH:MM:SS"
+     start: datetime.datetime | None  # In a POST request, it should be a string of the following format: "YYYY-MM-DD[T]HH:MM:SS"
+     end: datetime.datetime | None  # In a POST request, it should be a string of the following format: "YYYY-MM-DD[T]HH:MM:SS"
+     importance: int
+     user_id: int
+
+
 @app.get('/get_user/')
-def get_user(username: str="", email: str=""):
+def get_user(username="", email=""):
      if username == "" and email == "":
           return JSONResponse({"reason": "Neither username not email were provided."}, status_code=400)
      if username == "":
@@ -27,48 +45,68 @@ def get_user(username: str="", email: str=""):
 
 
 @app.post('/add_user/')
-def add_user(username: str, email: str, password_hash: str):
+def add_user(user: User):
      # Check whether the username is valid
-     if not (3 <= len(username) <= 32):
+     if not (3 <= len(user.username) <= 32):
           return JSONResponse({"reason": "The username is not between 3 and 32 characters long"}, status_code=400)
-     cursor.execute(f"""SELECT * FROM Users WHERE Username = '{username}'""")
+     cursor.execute(f"""SELECT * FROM Users WHERE Username = '{user.username}'""")
      if len(cursor.fetchall()) > 0:
           return JSONResponse({"reason": "A user with this username already exists"}, status_code=400)
      
      # Check whether the email is valid
      EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-     if not re.search(EMAIL_REGEX, email):
+     if not re.search(EMAIL_REGEX, user.email):
           return JSONResponse({"reason": "The email is not in the format example@address.com"}, status_code=400)
-     cursor.execute(f"""SELECT * FROM Users WHERE EmailAddress = '{email}'""")
+     cursor.execute(f"""SELECT * FROM Users WHERE EmailAddress = '{user.email}'""")
      if len(cursor.fetchall()) > 0:
           return JSONResponse({"reason": "A user with this email address already exists"}, status_code=400)
      
      # Check whether the password hash is valid
-     if len(password_hash) != 64:
+     if len(user.password_hash) != 64:
           return JSONResponse({"reason": "The password hash is not 64 characters long"}, status_code=400)
      
      # Insert the data
-     try:
-          cursor.execute(f"""INSERT INTO Users VALUES (NULL, '{username}', '{email}', '{password_hash}')""")
-          # db.commit()  # Uncomment before deployment
-          return JSONResponse({}, status_code=201)
-     except Exception as e:
-          print(e)
-          return JSONResponse({"reason": "Unknown error when inserting the data"}, status_code=400)
+     cursor.execute(f"""INSERT INTO Users VALUES (NULL, '{user.username}', '{user.email}', '{user.password_hash}')""")
+     db.commit()  # Uncomment before deployment
+     return JSONResponse({}, status_code=201)
 
 
 @app.delete('/delete_user/')
-def delete_user(username: str="", email: str=""):
-     if username == "" and email == "":
-          return JSONResponse({"reason": "Neither username not email were provided."}, status_code=400)
-     if username == "":
-          cursor.execute(f"""DELETE FROM Users WHERE EmailAddress = '{email}'""")
-     elif email == "":
-          cursor.execute(f"""DELETE FROM Users WHERE Username = '{username}'""")
-     else:
-          cursor.execute(f"""DELETE FROM Users WHERE Username = '{username}' AND EmailAddress = '{email}'""")
+def delete_user(user_id: int):
+     cursor.execute(f"""DELETE FROM Users WHERE UserID = {user_id}""")
      return JSONResponse({})
 
+
+@app.post('/add_task/')
+def add_task(task: Task):
+     # Check whether the task name is valid
+     if not (3 <= len(task.name) <= 32):
+          return JSONResponse({"reason": "The username is not between 3 and 32 characters long"}, status_code=400)
+     
+     # Check whether the importance is valid
+     if not (0 <= task.importance <= 10):
+          return JSONResponse({"reason": "The importance is not between 0 and 10"}, status_code=400)
+     
+     # Check whether there is either a deadline or a start and an end date and time
+     if not (task.deadline or (task.start and task.end)):
+          return JSONResponse({"reason": "The task should either have a deadline or a start and an end date and time"}, status_code=400)
+     
+     # Check whether the user with this ID exists
+     cursor.execute(f"""SELECT * FROM Users WHERE UserID = '{task.user_id}'""")
+     if len(cursor.fetchall()) == 0:
+          return JSONResponse({"reason": "The user with this ID does not exist"}, status_code=400)
+     
+     # Insert the data
+     if task.deadline:
+          deadline = task.deadline.strftime("%Y-%m-%d %H:%M:%S")
+          print(deadline)
+          cursor.execute(f"""INSERT INTO Tasks VALUES (NULL, '{task.name}', '{task.description}', '{deadline}', NULL, NULL, {task.importance}, {task.user_id})""")
+     elif task.start and task.end:
+          start = task.start.strftime("%Y-%m-%d %H:%M:%S")
+          end = task.end.strftime("%Y-%m-%d %H:%M:%S")
+          cursor.execute(f"""INSERT INTO Tasks VALUES (NULL, '{task.name}', '{task.description}', NULL, '{start}', '{end}', {task.importance}, {task.user_id})""")
+     db.commit()  # Uncomment before deployment
+     return JSONResponse({}, status_code=201)
 
 
 if __name__ == "__main__":
